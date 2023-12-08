b316d425f9b86ad2d1470b5bafa385b59469437c
sv <sgonzalezve@unal.edu.co>
2023-12-08 18:28:14 -0500

    Update Tests Commit on Functions

--- R/Ruda.R
+++ R/Ruda.R
@@ -1,5 +1,34 @@
 library(devtools)
 library(roxygen2)
+
+#' Function Cuadrado
+#'
+#' @param x An input number to calculate the square
+#'
+#' @return the square input number
+#' @export
+#'
+
 cuadrado = function(x){ return(x^2) }
+#' Function Cubo
+#'
+#' @param x An input number to calculate the cubic power
+#'
+#' @return the cube input number
+#' @export
+#'
+
 cubo     = function(x){ return(x^3) }
+#' Function Inverso
+#'
+#' @param x An input number to calculate the inverse power
+#'
+#' @return  the inverse power input number
+#' @export
+#'
+
 inverso_multi  = function(x){ return(x^(-1))}
+
+
+
+use_package("dplyr")
---
+++ tests/testthat/test-cubo.R
@@ -0,0 +1,3 @@
+test_that("Cubo works", {
+  expect_equal(cubo(3), 27)
+})
